a2c55a886b924827cb79b1b527ef7ffb43e8c61d
Laser Yuan <laser.yuan@gmail.com>
2021-07-21 05:08:24 +0000

    fix (tmux): did not persist $PATH

--- bin/open_tmux_vim.sh
+++ bin/open_tmux_vim.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 SESSION_NAME=${HOST_NAME:-0}
 
-tmux new-session -d -s "$SESSION_NAME" -n home
+tmux new-session -d -s "$SESSION_NAME" -n home bash
 tmux send-keys -t "$SESSION_NAME":home "vim" Enter
 tmux send-keys -t "$SESSION_NAME":home "C-g" Enter
