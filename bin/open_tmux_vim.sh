4c6053d875305419e79886ff10adbab97815479c
Laser Yuan <laser.yuan@gmail.com>
2022-06-26 12:35:50 +0000

    fix: vim can't be open in tmux automatically

--- bin/open_tmux_vim.sh
+++ bin/open_tmux_vim.sh
@@ -3,4 +3,5 @@ SESSION_NAME=${HOST_NAME:-0}
 
 tmux new-session -d -s "$SESSION_NAME" -n home bash
 tmux send-keys -t "$SESSION_NAME":home "vim" Enter
-tmux send-keys -t "$SESSION_NAME":home "C-g" Enter
+sleep 3
+tmux send-keys -t "$SESSION_NAME":home "C-g"
