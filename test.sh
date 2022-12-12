098b05cf178f775e423c50177109dab7549d86f3
Laser Yuan <laser.yuan@gmail.com>
2022-12-12 07:07:29 +0000

    feat (sshstart): add --restart tag

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+bash /usr/local/bin/sshstart test
 /usr/local/bin/sshyou test
 /home/ride/.dotfiles/bash/.bashrc.d/.dockerfunc.sh test
 python3 -m pytest ./mbuild/utils/build.py
