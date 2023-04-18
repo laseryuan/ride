be628fbc7b8c244517fa22c6b2cdf5aa4621dcb5
Laser Yuan <laser.yuan@gmail.com>
2023-04-18 05:23:04 +0000

    fix: ride user can't be add to host docker group when the group name is not called docker

--- bin/enable-docker.sh
+++ bin/enable-docker.sh
@@ -4,5 +4,5 @@
 if [ -n "${HOST_DOCKER_ID}" ]; then
     echo "Adding user to host docker group id" >> /tmp/ride.log;
     groupadd --force --gid $HOST_DOCKER_ID docker
-    usermod -a -G docker ride
+    usermod -a -G $HOST_DOCKER_ID ride
 fi
