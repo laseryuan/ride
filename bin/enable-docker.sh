dd34f15c18080045438192927d15a02374ab4a9c
Laser Yuan <laser.yuan@gmail.com>
2023-11-04 07:13:49 +0800

    feat: can run docker in macos

--- bin/enable-docker.sh
+++ bin/enable-docker.sh
@@ -5,4 +5,11 @@ if [ -n "${HOST_DOCKER_ID}" ]; then
     # echo "Adding user to host docker group id" >> /tmp/ride.log;
     groupadd --force --gid $HOST_DOCKER_ID docker
     usermod -a -G $HOST_DOCKER_ID ride
+else
+    # macos docker user is root
+    # https://github.com/docker/for-mac/issues/5072
+    groupadd docker
+    usermod -aG docker ride 
+    chgrp docker /var/run/docker.sock
+    chmod g+w /var/run/docker.sock
 fi
