1a2cad570b5c5a6351005ac484c9a501de3343f0
Laser Yuan <laser.yuan@gmail.com>
2025-04-10 16:05:58 -0700

    feat: can use ride config from host

--- bin/config-home.sh
+++ bin/config-home.sh
@@ -3,3 +3,7 @@
 if [ -f "$HOME/.ride/chiff/session" ]; then
     ln -sf "$HOME/.ride/chiff/session" "$HOME/.config/chiff/session"
 fi
+
+if [ -f "$HOME/.ride/ssh/config" ]; then
+    ln -sf "$HOME/.ride/ssh/config" "$HOME/.ssh/config"
+fi
